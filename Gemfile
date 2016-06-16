9f76e2be34c92a0809a7ac33706fa18fd552e176
Markus Springer <markus.springer1994@googlemail.com>
2016-06-16 09:05:35 +0200

    First implementation of coloranalyzer

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'aruba'
+
 # Specify your gem's dependencies in coloranalyzer.gemspec
 gemspec
--- lib/coloranalyzer.rb
+++ lib/coloranalyzer.rb
@@ -1,5 +1,15 @@
 require "coloranalyzer/version"
 
+require 'rubygems'
+require 'RMagick'
+include Magick
+
+
 module Coloranalyzer
-  # Your code goes here...
+    def self.getHexFromFile(filepath)
+      img =  Magick::Image.read(filepath).first
+      pix = img.scale(1, 1)
+      avg_color_hex = pix.to_color(pix.pixel_color(0,0))
+      return avg_color_hex
+    end
 end
